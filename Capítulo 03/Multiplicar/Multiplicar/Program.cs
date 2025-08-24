6dd6e5232b40c6d4abf384fdc67caa5e580d6a4e
Natalia da Silva Reis <nataliadasilvareis@gmail.com>
2025-08-24 11:07:02 -0300

    Finalizado o primeiro exercício.

---
+++ Capítulo 03/Multiplicar/Multiplicar/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Multiplicar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Multiplicar(int fator1, int fator2)
+            {
+                int produto = fator1 * fator2;
+
+                return produto;
+            }
+
+            int altura = 179;
+            int largura = 83;
+            int area = Multiplicar(altura, largura);
+        }
+    }
+}
