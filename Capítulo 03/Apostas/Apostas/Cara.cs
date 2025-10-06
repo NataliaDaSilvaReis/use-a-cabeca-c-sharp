56535c979943b79344c8ff5f709b2830b03d0cf8
Natalia da Silva Reis <nataliadasilvareis@gmail.com>
2025-10-06 09:07:54 -0300

    Finalizado o exercício Apostas do capítulo 03. Fim do capítulo.

---
+++ Capítulo 03/Apostas/Apostas/Cara.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apostas
+{
+    internal class Cara
+    {
+        public string _nome;
+        public double _dinheiro;
+
+
+        /// <summary>
+        /// Escreve o nome e a quantia de dinheiro no console.
+        /// </summary>
+        public void EscrevaMinhasInformacoes()
+        {
+            Console.WriteLine($"{_nome} tem {_dinheiro} reais.");
+        }
+
+
+        /// <summary>
+        /// Dá parte do dinheiro, retirando-o da carteira (ou escrevendo uma mensagem no console se não houver dinheiro suficiente).
+        /// </summary>
+        /// <param name="quantia">Quantia de dinheiro a entregar.</param>
+        /// <returns>
+        /// A quantia de dinheiro removida da carteira ou 0 se não houver dinheiro suficiente (ou se a quantia é inválida).
+        /// </returns>
+        public double DarDinheiro(double quantia)
+        {
+            if (quantia <= 0)
+            {
+                Console.WriteLine($"{_nome} diz: {quantia} não é um valor válido.");
+
+                return 0;
+            }
+
+            if (quantia > _dinheiro)
+            {
+                Console.WriteLine($"{_nome} diz: Não tenho dinheiro suficiente para lhe dar {quantia}");
+
+                return 0;
+            }
+
+            _dinheiro -= quantia;
+
+            return quantia;
+        }
+
+
+        /// <summary>
+        /// Recebe algum dinheiro, adicionando-o a carteira (ou escrevendo uma mensagem no console se a quantia é inválida).
+        /// </summary>
+        /// <param name="quantia">Quantia de dinheiro a entregar.</param>
+        public void ReceberDinheiro(double quantia)
+        {
+            if (quantia <= 0)
+            {
+                Console.WriteLine($"{_nome} diz: {quantia} não é uma quantia que eu aceitaria.");
+            }
+            else
+            {
+                _dinheiro += quantia;
+            }
+        }
+    }
+}
---
+++ Capítulo 03/Apostas/Apostas/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Apostas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            const double _chances = 0.75;
+            Cara jogador = new Cara() { _dinheiro = 100, _nome = "O jogador" };
+
+
+            Console.WriteLine($"Bem-vindo ao cassino. As probabilidades são de {_chances}.");
+            Console.WriteLine();
+
+            while (jogador._dinheiro > 0)
+            {
+                jogador.EscrevaMinhasInformacoes();
+                Console.Write("Quanto você quer apostar: ");
+                string quanto = Console.ReadLine();
+
+                if (double.TryParse(quanto, out double quantia))
+                {
+                    double pote = jogador.DarDinheiro(quantia) * 2;
+
+                    if (pote > 0)
+                    {
+                        if (random.NextDouble() > _chances)
+                        {
+                            jogador.ReceberDinheiro(pote);
+                            Console.WriteLine($"Você ganhou {pote}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Má sorte, você perdeu.");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Por favor, digite um número válido.");
+                }
+            }
+
+            Console.WriteLine("A casa sempre ganha.");
+        }
+    }
+}
