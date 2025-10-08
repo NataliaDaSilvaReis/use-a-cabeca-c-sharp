00b796e951c13fd60d60a35cb91a89a13fa537ef
Natalia da Silva Reis <nataliadasilvareis@gmail.com>
2025-10-08 08:17:31 -0300

    Finalizado o exercício Testador de Pontuação de Habilidade do Capítulo 04.

---
+++ Capítulo 04/TestadorDePontuacaoDeHabilidade/TestadorDePontuacaoDeHabilidade/CalculadoraDePontuacaoDeHabilidade.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestadorDePontuacaoDeHabilidade
+{
+    internal class CalculadoraDePontuacaoDeHabilidade
+    {
+        public int ResultadoDoLancamento = 14;
+        public double DividirPor = 1.75;
+        public int AddValor = 2;
+        public int Minimo = 3;
+        public int _pontuacao;
+
+        public void CalcularPontuacaoDeHabilidade()
+        {
+
+            // Divida o resultado pelo campo DividirPor
+            double dividido = ResultadoDoLancamento / DividirPor;
+
+            // Arredonde para baixo o resultado da divisão e adicione AddValor
+            int adicionado = AddValor + (int)dividido;
+
+            // Se o resultado for muito pequeno, use o Minimo
+            if (adicionado < Minimo)
+            {
+                _pontuacao = Minimo;
+            }
+            else
+            {
+                _pontuacao = adicionado;
+            }
+
+        }
+    }
+}
---
+++ Capítulo 04/TestadorDePontuacaoDeHabilidade/TestadorDePontuacaoDeHabilidade/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Drawing;
+
+namespace TestadorDePontuacaoDeHabilidade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CalculadoraDePontuacaoDeHabilidade calculadora = new CalculadoraDePontuacaoDeHabilidade();
+
+
+            while (true)
+            {
+                calculadora.ResultadoDoLancamento = LeiaInt(calculadora.ResultadoDoLancamento, "Rolagem inicial de 4d6");
+                calculadora.DividirPor = LeiaDouble(calculadora.DividirPor, "Dividir por");
+                calculadora.AddValor = LeiaInt(calculadora.AddValor, "Adicionar valor");
+                calculadora.Minimo = LeiaInt(calculadora.Minimo, "Mínimo");
+
+                calculadora.CalcularPontuacaoDeHabilidade();
+
+                Console.WriteLine("Pontuação de habilidade calculada: " + calculadora._pontuacao);
+                Console.WriteLine("Pressione Q para sair, qualquer outra tecla para continuar");
+                char caractereChar = Console.ReadKey(true).KeyChar;
+
+                if ((caractereChar == 'Q') || (caractereChar == 'q')) return;
+            }
+
+
+            static int LeiaInt(int ultimoValorUsado, string prompt)
+            {
+
+                Console.Write($"{prompt} [{ultimoValorUsado}]: ");
+                string linha = Console.ReadLine();
+
+                if (int.TryParse(linha, out int valor))
+                {
+                    Console.WriteLine("    usando valor " + valor);
+                    return valor;
+                }
+                else
+                {
+                    Console.WriteLine("    usando o valor padrão " + ultimoValorUsado);
+                    return ultimoValorUsado;
+                }
+            }
+
+
+            static double LeiaDouble(double ultimoValorUsado, string prompt)
+            {
+
+                Console.Write($"{prompt} [{ultimoValorUsado}]: ");
+                string linha = Console.ReadLine();
+
+                if (double.TryParse(linha, out double valor))
+                {
+                    Console.WriteLine("    usando valor " + valor);
+                    return valor;
+                }
+                else
+                {
+                    Console.WriteLine("    usando o valor padrão " + ultimoValorUsado);
+                    return ultimoValorUsado;
+                }
+            }
+        }
+    }
+}
