5e05e42b14deb12311697eb914da081553442878
Natalia da Silva Reis <nataliadasilvareis@gmail.com>
2025-10-10 09:35:11 -0300

    Finalizado o exercício Sandubas do capítulo 04.

---
+++ Capítulo 04/SandubasPrecoBomEAqui/SandubasPrecoBomEAqui/ItemDoMenu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandubasPrecoBomEAqui
+{
+    internal class ItemDoMenu
+    {
+        public static Random randomizar = new Random();
+
+        public string[] proteinas = { "Carne assada", "Salame", "Peru", "Presunto", "Pastrami", "Tofu" };
+        public string[] condimentos = { "mostarda amarela", "mostarda escura", "mostarda e mel", "maionese", "relish", "molho french" };
+        public string[] paes = { "pão de centeio", "pão branco", "pão de trigo", "pão integral", "pão de rolo" };
+
+        public string descricao = "";
+        public string preco;
+
+
+
+        public void Gerar()
+        {
+            string randomProteina = proteinas[randomizar.Next(proteinas.Length)];
+            string randomCondimento = condimentos[randomizar.Next(condimentos.Length)];
+            string randomPao = paes[randomizar.Next(paes.Length)];
+
+            descricao = $"{randomProteina} com {randomCondimento} no {randomPao}";
+
+            decimal reais = randomizar.Next(2, 5);
+            decimal centavos = randomizar.Next(1, 98);
+            decimal valor = reais + (centavos * .01M);
+            preco = valor.ToString("c");
+        }
+    }
+}
---
+++ Capítulo 04/SandubasPrecoBomEAqui/SandubasPrecoBomEAqui/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SandubasPrecoBomEAqui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FacaOMenu();
+        }
+
+
+
+        private void FacaOMenu()
+        {
+            ItemDoMenu[] itensDoMenu = new ItemDoMenu[5];
+            string precoGuacamole;
+
+            for (int i = 0; i < 5; i++)
+            {
+                itensDoMenu[i] = new ItemDoMenu();
+
+                if (i >= 3)
+                {
+                    itensDoMenu[i].paes = new string[] { "bagel simples", "bagel de cebola", "bagel integral", "tudo bagel" };
+                }
+
+                itensDoMenu[i].Gerar();
+
+            }
+
+            item1.Text = itensDoMenu[0].descricao;
+            preco1.Text = itensDoMenu[0].preco;
+            item2.Text = itensDoMenu[1].descricao;
+            preco2.Text = itensDoMenu[1].preco;
+            item3.Text = itensDoMenu[2].descricao;
+            preco3.Text = itensDoMenu[2].preco;
+            item4.Text = itensDoMenu[3].descricao;
+            preco4.Text = itensDoMenu[3].preco;
+            item5.Text = itensDoMenu[4].descricao;
+            preco5.Text = itensDoMenu[4].preco;
+
+            ItemDoMenu itemEspecialDoMenu = new ItemDoMenu()
+            {
+                proteinas = new string[] { "Presunto orgânico", "Hambúrguer de cogumelo", "Mortadela" },
+                paes = new string[] { "pãozinho sem glúten", "wrap", "pão pita" },
+                condimentos = new string[] { "mostarda dijon", "missô", "au jus" }
+            };
+
+            itemEspecialDoMenu.Gerar();
+
+            item6.Text = itemEspecialDoMenu.descricao;
+            preco6.Text = itemEspecialDoMenu.preco;
+
+            ItemDoMenu itemDoMenuGuacamole = new ItemDoMenu();
+            itemDoMenuGuacamole.Gerar();
+            precoGuacamole = itemDoMenuGuacamole.preco;
+
+            guacamole.Text = "Adicione guacamole por: " + itemDoMenuGuacamole.preco;
+
+        }
+    }
+}
