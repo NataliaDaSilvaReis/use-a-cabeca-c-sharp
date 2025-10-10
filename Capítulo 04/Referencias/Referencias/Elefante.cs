7c8dafe9d1dbfe72e4ccaa22734a44aa9c8d5b48
Natalia da Silva Reis <nataliadasilvareis@gmail.com>
2025-10-10 07:14:37 -0300

    Finalizado o exercício de Referências do capítulo 04.

---
+++ Capítulo 04/Referencias/Referencias/Elefante.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Referencias
+{
+    internal class Elefante
+    {
+
+        public string _nome;
+        public int _tamanhoOrelha;
+
+        public void QuemSouEu()
+        {
+
+            Console.WriteLine($"Meu nome é {_nome}.");
+            Console.WriteLine($"Minhas orelhas têm {_tamanhoOrelha} centímetros de altura.");
+
+        }
+
+
+        public void OuvirMensagem(string mensagem, Elefante quemDisseIsso)
+        {
+
+            Console.WriteLine($"{_nome} ouviu a mensagem");
+            Console.WriteLine($"{quemDisseIsso._nome} disse isso: {mensagem}");
+
+        }
+
+
+        public void FalarCom(Elefante comQuemFalar, string mensagem)
+        {
+
+            comQuemFalar.OuvirMensagem(mensagem, this);
+
+        }
+
+    }
+}
---
+++ Capítulo 04/Referencias/Referencias/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Referencias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Elefante lucinda = new Elefante() { _nome = "Lucinda", _tamanhoOrelha = 84 };
+            Elefante lloyd = new Elefante() { _nome = "Lloyd", _tamanhoOrelha = 102 };
+            Elefante auxiliar;
+
+
+            Console.WriteLine("Pressione 1 para Lloyd, 2 para Lucinda, 3 para trocar");
+
+            while (true)
+            {
+
+                char linha = Console.ReadKey(true).KeyChar;
+
+                if (int.TryParse(linha.ToString(), out int opcao))
+                {
+                    Console.WriteLine($"Você pressionou {opcao}");
+
+                    if (opcao == 1)
+                    {
+
+                        Console.WriteLine("Chamando lloyd.QuemSouEu()");
+                        lloyd.QuemSouEu();
+
+                    }
+                    else if (opcao == 2)
+                    {
+
+                        Console.WriteLine("Chamando lucinda.QuemSouEu()");
+                        lucinda.QuemSouEu();
+
+                    }
+                    else if (opcao == 3)
+                    {
+
+                        auxiliar = lloyd;
+                        lloyd = lucinda;
+                        lucinda = auxiliar;
+
+                        Console.WriteLine("As referências foram trocadas");
+
+                    }
+                    else if (opcao == 4)
+                    {
+
+                        lloyd = lucinda;
+                        lloyd._tamanhoOrelha = 4321;
+                        lloyd.QuemSouEu();
+
+                    }
+                    else if (opcao == 5)
+                    {
+
+                        lucinda.FalarCom(lloyd, "Olá, Lloyd!");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pressione uma opção válida.");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Pressione uma opção válida.");
+                }
+
+                Console.WriteLine();
+
+            }
+
+        }
+
+    }
+}
