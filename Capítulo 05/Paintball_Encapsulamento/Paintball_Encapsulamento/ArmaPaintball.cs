f77e68b068f9f7597f463f1e9cce6ca49420472c
Natalia da Silva Reis <nataliadasilvareis@gmail.com>
2025-10-17 10:13:48 -0300

    Início do exercício Paintball do capítulo 05, sobre propriedades e encapsulamento.

---
+++ Capítulo 05/Paintball_Encapsulamento/Paintball_Encapsulamento/ArmaPaintball.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paintball_Encapsulamento
+{
+    internal class ArmaPaintball
+    {
+
+        public const int TAMANHO_DO_CARREGADOR = 16;
+
+        private int bolas = 0;
+        private int bolasCarregadas = 0;
+
+
+
+        // Início dos métodos Getters e Setters.
+        public int GetBolasCarregadas()
+        {
+            return bolasCarregadas;
+        }
+
+        public int Bolas
+        {
+            get { return bolas; }
+
+            set
+            {
+                if (value > 0)
+                {
+                    bolas = value;
+                }
+
+                Recarregar();
+            }
+        }
+        // Fim dos métodos Getters e Setters.
+
+
+
+        public bool SemMunicao()
+        {
+            return bolasCarregadas == 0;
+        }
+
+        public void Recarregar()
+        {
+
+            if (bolas > TAMANHO_DO_CARREGADOR)
+            {
+
+                bolasCarregadas = TAMANHO_DO_CARREGADOR;
+
+            }
+            else
+            {
+
+                bolasCarregadas = bolas;
+
+            }
+        }
+
+        public bool Atirar()
+        {
+
+            if (bolasCarregadas == 0) return false;
+
+            bolasCarregadas--;
+            bolas--;
+
+            return true;
+
+        }
+    }
+}
---
+++ Capítulo 05/Paintball_Encapsulamento/Paintball_Encapsulamento/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Paintball_Encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArmaPaintball arma = new ArmaPaintball();
+
+
+
+            while (true)
+            {
+
+                Console.WriteLine($"{arma.GetBolas()} bolas, {arma.GetBolasCarregadas()} carregadas");
+
+                if (arma.SemMunicao()) Console.WriteLine("AVISO: você está sem munição");
+
+                Console.WriteLine("Barra de espaço para atirar, r para recarregar, + para adicionar munição, s para sair\n");
+                char opcao = Console.ReadKey(true).KeyChar;
+
+                if (opcao == ' ') Console.WriteLine($"Atirar retornou {arma.Atirar()}");
+                else if (opcao == 'r') arma.Recarregar();
+                else if (opcao == '+') arma.SetBolas(arma.GetBolas() + ArmaPaintball.TAMANHO_DO_CARREGADOR);
+                else if (opcao == 's') return;
+
+            }
+        }
+    }
+}
